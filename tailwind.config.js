0b5b3e8649c4e81c06f8479c5219b69a1281a9af
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2024-10-17 15:57:48 +0200

    Custom styles and cleaning files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
     extend: {},
     colors: {
-      'main-blue': '#0A435D',
+      'main-bg': '#0A435D',
+      'main': '#08354a',
       'secondary-blue': '#00A0F4',
       'light-blue': '#00BDFF',
       'orange': '#EA9A28',
       'green-lime': '#CBF279'
     },
+    fontFamily: {
+      montserrat: ['Montserrat', 'sans-serif'],
+      della: ['Della Respira', 'serif'],
+      hind: ['Hind', 'sans-serif'],
+      proza: ['Proza Libre', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
