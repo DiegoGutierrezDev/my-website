06bd58c5a585969368ea04d18dfb83be9b84eff3
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2024-10-30 14:22:24 +0100

    Structuring project, images and styles

---
+++ src/components/customHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="webdev">
+        <a href="">
+            <span class="name">Diego Gutiérrez</span>
+                <span class="developer">Web Developer</span>
+        </a>
+    </div>
+
+    <div class="social">
+
+        <a href="mailto:gutierrezmaria.diego@gmail.com" class="social-img">
+            <img src="/src/assets/img/email.png" alt="email"></a>
+
+        <a href="https://github.com/DiegoGutierrezDev" class="social-img" target="_blank">
+            <img src="/src/assets/img/github.png" alt="github"></a>
+
+        <a href="https://www.linkedin.com/in/diegoguti%C3%A9rrezdev/" class="social-img" target="_blank">
+            <img src="/src/assets/img/linkedin.png" alt="linkedin"></a>
+    </div>
+</template>
+
+
+  
+  <script>
+  export default {
+    data() {
+      return {
+        who: "World",
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  span {
+    color: blue;
+  }
+  </style>
