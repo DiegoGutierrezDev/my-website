1ab5076f93849963140a8a103894cf027eeef362
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2024-10-30 18:07:00 +0100

    Little fixes in styles

--- src/components/customHeader.vue
+++ src/components/customHeader.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="flex">
-     <div class="webdev">
-        <a href="">
+  <div class="flex justify-between">
+     <div class="text-center">
+        <RouterLink href="/">
             <h1>Diego Gutiérrez</h1>
                 <h2>Web Developer</h2>
-        </a>
+        </RouterLink>
     </div>
 
-    <div class="flex w-full justify-end items-end">
+    <div class="flex items-end">
 
         <a href="mailto:gutierrezmaria.diego@gmail.com" class="img-links">
             <img src="/src/assets/img/email.png" alt="email"></a>
 
-        <a href="https://github.com/DiegoGutierrezDev" class="img-links" target="_blank">
-            <img src="/src/assets/img/github.png" alt="github"></a>
+        <RouterLink href="https://github.com/DiegoGutierrezDev" class="img-links" target="_blank">
+            <img src="/src/assets/img/github.png" alt="github"></RouterLink>
 
-        <a href="https://www.linkedin.com/in/diegoguti%C3%A9rrezdev/" class="img-links" target="_blank">
-            <img src="/src/assets/img/linkedin.png" alt="linkedin"></a>
+        <RouterLink href="https://www.linkedin.com/in/diegoguti%C3%A9rrezdev/" class="img-links" target="_blank">
+            <img src="/src/assets/img/linkedin.png" alt="linkedin"></RouterLink>
     </div>
   </div>
    
