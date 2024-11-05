a78a222a080722bfb7787ee0ee4d14b080f55b66
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2024-11-05 18:50:27 +0100

    Fix for deployment

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import customHeader from './components/CustomHeader.vue';
+import customHeader from './components/customHeader.vue';
 import NavigationBar from './components/navigationBar.vue';
 </script>
 
