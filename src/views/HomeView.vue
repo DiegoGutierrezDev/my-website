546236e9a49079d415d76157ea86eb9930524fac
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-18 15:46:11 +0100

    Fix for font

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="description" id="description">
             <div>
-                <span class="max-md:hidden">
+                <span class="max-md:hidden font-hind">
                     He encontrado mi vocación en el mundo de la programación, me encanta aprender y seguir siempre practicando, me encanta la sensación de mejora y de sentir el progreso, el uso de la lógica y el constante rompecabezas que es la programación.
                     <br> <br>
                     Empecé a estudiar de forma autodidacta en 2018 y desde entonces no he parado, es algo que desde el principio me enganchó y que me gusta mucho practicar, desde entonces he realizado múltiples cursos, un Grado Superior de Administración de Sistemas Informáticos en Red y actualmente estoy realizando dos cursos y un portafolio, además de la búsqueda de empleo, te interesa?
