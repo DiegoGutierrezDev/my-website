c27545e6eeab10ff0ff18880a5cfd967485f9cb0
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-20 20:07:51 +0100

    Cards for titles laid out

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="blue-background grid grid-cols-4 justify-between justify-items-center">
-    <div v-for="title in certificates" class="frames">
-      <VuePdfEmbed :source="title.src"></VuePdfEmbed>
+    <div v-for="title in certificates" class="flex items-center justify-center rounded-lg mb-8">
+      <div class="">
+        <img src="../assets/img/frame.png" 
+          alt="" 
+          class="absolute z-40" 
+          style="width: 335px; height: 253px;"
+          >
+        <VuePdfEmbed 
+          :source="title.src" 
+          class="mt-5 ml-1" 
+          style="width: 328px;">
+        </VuePdfEmbed>
+      </div>
     </div>
   </div>
 </template>
@@ -12,32 +23,19 @@ import VuePdfEmbed from 'vue-pdf-embed';
 
 // Data
 const certificates = ref([
-  {title: 'Javascript', year: '2018', src: "src/assets/titles/prueba.pdf"},
-  {title: 'Javascript', year: '2019'},
-  {title: 'Javascript', year: '2012'},
-  {title: 'Javascript', year: '2015'},
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2019', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2012', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2015', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2019', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2012', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2015', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2019', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2012', src: "src/assets/titles/AppsMoviles.pdf"},
+  {title: 'Javascript', year: '2015', src: "src/assets/titles/AppsMoviles.pdf"},
 ])
 
-const pdfSource = 'src'
-
-// Functions
-
-
 </script>
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
 
-.frames{
-  background-image: url(../assets/img/frame.png);
-  background-size: cover;
-  width: 15em;
-  height: 15em;
-  display: inline-block;
-}
-</style>
