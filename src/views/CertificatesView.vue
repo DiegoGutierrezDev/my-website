7663f9964d2a1c2a81223b3751f1300246805cd6
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-20 17:16:40 +0100

    VuePdfEmbed library installed and checked

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -1,10 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="blue-background grid grid-cols-4 justify-between justify-items-center">
+    <div v-for="title in certificates" class="frames">
+      <VuePdfEmbed :source="title.src"></VuePdfEmbed>
+    </div>
   </div>
 </template>
 
-<!-- <style>
+<script setup>
+import { ref } from 'vue'
+import VuePdfEmbed from 'vue-pdf-embed';
+
+// Data
+const certificates = ref([
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/prueba.pdf"},
+  {title: 'Javascript', year: '2019'},
+  {title: 'Javascript', year: '2012'},
+  {title: 'Javascript', year: '2015'},
+])
+
+const pdfSource = 'src'
+
+// Functions
+
+
+</script>
+<style>
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
@@ -12,4 +32,12 @@
     align-items: center;
   }
 }
-</style> -->
+
+.frames{
+  background-image: url(../assets/img/frame.png);
+  background-size: cover;
+  width: 15em;
+  height: 15em;
+  display: inline-block;
+}
+</style>
