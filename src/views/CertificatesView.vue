4d1d31b60c8f5218c0309b9d98a8ab27073b2ff6
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-25 12:10:02 +0100

    Optimized sizes, titles and years

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -2,17 +2,24 @@
   <div class="blue-background grid grid-cols-4 justify-between justify-items-center">
     <div v-for="title in certificates" class="flex items-center justify-center rounded-lg mb-8 pa-4">
       <div class="">
+        <h1>
+          {{ title.year }}
+        </h1>
+        <h2 class="mb-2">
+          {{ title.title }}
+        </h2>
+
         <img src="../assets/img/frame.png" 
           alt="" 
-          class="absolute z-40" 
-          style="width: 335px; height: 253px;"
+          class="absolute z-40 w-[335px] h-[260px]" 
           >
+        <div class="rounded-full">
           <img 
-          :src="title.src" 
-          alt="" 
-          class="pt-5 pl-2 pr-0.5 items-center flex justify-center" 
-          style="width: 330px; height: 250px;"
-          >
+            :src="title.src" 
+            alt="" 
+            class="pt-[1.37rem] pl-2 pr-0.5 w-[330px] h-[250px] rounded-lg" 
+            >
+        </div>
       </div>
     </div>
   </div>
@@ -23,18 +30,18 @@ import { ref } from 'vue'
 
 // Data
 const certificates = ref([
-  {title: 'Desarrollo de Apps Moviles', year: '2018', src: "src/assets/titles/AppsMoviles.png"},
-  {title: 'Básicos de Android', year: '2019', src: "src/assets/titles/BasicsAndroid.png"},
-  {title: 'How To Code', year: '2012', src: "src/assets/titles/HowtoCode.png"},
-  {title: 'HTML y CSS', year: '2015', src: "src/assets/titles/HTMLCss.png"},
-  {title: 'Desarrollo Web I', year: '2018', src: "src/assets/titles/WebDev1.png"},
-  {title: 'Javascript Asíncrono', year: '2019', src: "src/assets/titles/JavaScriptAsincrono.png"},
-  {title: 'Javascript', year: '2012', src: "src/assets/titles/JavaScript.png"},
-  {title: 'Laravel desde cero', year: '2015', src: "src/assets/titles/Laravel_MasterMind.png"},
-  {title: 'Python básico', year: '2018', src: "src/assets/titles/Python MasterMind.png"},
-  {title: 'Symfony 6 basic', year: '2019', src: "src/assets/titles/Symfony-6-Basic.png"},
-  {title: 'Desarrollo con Vue.js y Laravel', year: '2012', src: "src/assets/titles/Vue&Laravel MasterMind.png"},
-  {title: 'Desarrollo con Vue.js', year: '2015', src: "src/assets/titles/Vue.jsMasterMind.png"},
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/JavaScript.png"},
+  {title: 'Desarrollo Web I', year: '2019', src: "src/assets/titles/WebDev1.png"},
+  {title: 'Desarrollo de Apps Moviles', year: '2019', src: "src/assets/titles/AppsMoviles.png"},
+  {title: 'Básicos de Android', year: '2020', src: "src/assets/titles/BasicsAndroid.png"},
+  {title: 'How To Code', year: '2020', src: "src/assets/titles/HowtoCode.png"},
+  {title: 'Javascript Asíncrono', year: '2020', src: "src/assets/titles/JavaScriptAsincrono.png"},
+  {title: 'HTML y CSS', year: '2022', src: "src/assets/titles/HTMLCss.png"},
+  {title: 'Laravel desde cero', year: '2023', src: "src/assets/titles/Laravel_MasterMind.png"},
+  {title: 'Python básico', year: '2023', src: "src/assets/titles/Python MasterMind.png"},
+  {title: 'Symfony 6 basic', year: '2023', src: "src/assets/titles/Symfony-6-Basic.png"},
+  {title: 'Desarrollo con Vue.js y Laravel', year: '2023', src: "src/assets/titles/Vue&Laravel MasterMind.png"},
+  {title: 'Desarrollo con Vue.js', year: '2023', src: "src/assets/titles/Vue.jsMasterMind.png"},
 ])
 
 </script>
