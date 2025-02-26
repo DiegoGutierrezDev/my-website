90ec5315c199e944b238d72a59dd84ea68fcb591
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-26 18:03:10 +0100

    Hotfix for deployment

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/my-website/',
+  base: 'diegogutierrezdev.netlify.app',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
