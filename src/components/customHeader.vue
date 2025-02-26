323ca7eb345263fcb264c8cfa63d287bed84136c
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-02-26 17:33:00 +0100

    Button for downloading CV

--- src/components/customHeader.vue
+++ src/components/customHeader.vue
@@ -1,14 +1,15 @@
+<script setup>
+import CustomButton from './customButton.vue';
+</script>
+
 <template>
   <div class="bg-main flex justify-between items-center px-5 py-2">
      <div class="text-center">
         <RouterLink to="/" class="flex items-center" id="avoidHover">
             <img src="../assets/img/CV.png" alt="" class="rounded-full w-16 mr-5">
-            <div>
-                <h1>Diego Gutiérrez</h1>
-            </div>
-            
+            <h1>Diego Gutiérrez</h1>
         </RouterLink>
-    </div>
+        </div>
 
     <div class="w-96 flex justify-around items-center font-montserrat font-semibold text-orange">
         <router-link to="/">Inicio</router-link>
@@ -18,6 +19,13 @@
 
     <div class="flex items-end">
 
+        <a href="/src/assets/documents/CV.pdf" download="cv-diego-gutierrez-2025.pdf">
+            <CustomButton text="Descargar CV">
+                <template v-slot:icon>
+                    <img src="@/assets/img/download.svg" alt="" class="w-4 ml-1">
+                </template>
+            </CustomButton>
+        </a>
         <a href="mailto:gutierrezmaria.diego@gmail.com" class="img-links">
             <img src="/src/assets/img/email.png" alt="email"></a>
 
