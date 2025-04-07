cd24cddb1faf041ce09b180dcaaacaca2fc908a8
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-04-07 15:19:32 +0200

    More interactive links and UI changes

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -21,7 +21,7 @@ import { stack, carreer } from '@/constants/data';
 
         <div class="flex flex-col items-center justify-center md:justify-start md:flex-row">
 
-        <a href="/src/assets/documents/CV.pdf" download="cv-diego-gutierrez-2025.pdf" class="md:mr-10">
+        <a href="/documents/CV.pdf" download="cv-diego-gutierrez-2025.pdf" class="md:mr-10">
             <AppButton>
                 <template #content>
                     <span class="mr-3 text-black">
@@ -40,7 +40,8 @@ import { stack, carreer } from '@/constants/data';
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke-width="2"
-                stroke="currentColor"
+                stroke="white"
+                class="hover:stroke-[#66fcf1]"
             >
                 <path
                 stroke-linecap="round"
@@ -54,7 +55,8 @@ import { stack, carreer } from '@/constants/data';
             <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 18 16"
-            fill="currentColor"
+            fill="white"
+            class="hover:fill-[#66fcf1]"
             >
             <path
             d="M16.5128 0H0.717949C0.287179 0 0 0.266667 0 0.666667V15.3333C0 15.7333 0.287179 16 0.717949 16H16.5128C16.9436 16 17.2308 15.7333 17.2308 15.3333V0.666667C17.2308 0.266667 16.9436 0 16.5128 0ZM5.09744 13.6667H2.58462V6H5.16923V13.6667H5.09744ZM3.80513 4.93333C3.01538 4.93333 2.29744 4.33333 2.29744 3.53333C2.29744 2.8 2.94359 2.13333 3.80513 2.13333C4.59487 2.13333 5.31282 2.73333 5.31282 3.53333C5.31282 4.33333 4.66667 4.93333 3.80513 4.93333ZM14.7179 13.6667H12.1333V9.93333C12.1333 9.06667 12.1333 7.93333 10.841 7.93333C9.47692 7.93333 9.33333 8.86667 9.33333 9.86667V13.6667H6.74872V6H9.18974V7.06667C9.54872 6.46667 10.3385 5.86667 11.6308 5.86667C14.2154 5.86667 14.7179 7.46667 14.7179 9.53333V13.6667Z"
@@ -65,8 +67,9 @@ import { stack, carreer } from '@/constants/data';
           <a href="https://github.com/DiegoGutierrezDev" target="_blank" class="size-6">
             <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
+            fill="white"
             viewBox="0 0 24 24"
+            class="hover:fill-[#66fcf1]"
         >
             <path
             d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
@@ -95,10 +98,14 @@ import { stack, carreer } from '@/constants/data';
       <div class="flex" :class=" index % 2 == 0 ? 'justify-start' : 'justify-end'">
         <div class="md:w-1/2 " :class=" index % 2 == 0 ? 'md:pr-10' : 'md:pl-10'">
           <div class="items-end flex gap-3" :class="index % 2 != 0 ? 'md:justify-start' : 'md:justify-end'">
-            <span class=" text-main-blue font-semibold text-2xl" 
-            :class="index % 2 != 0 ? 'md:order-1' : 'md:order-2'">{{ step.name }}</span>
+            <a :href="step.website" :target="step.external ? '_blank' : '_self'" class=" text-main-blue font-semibold text-2xl" 
+            :class="index % 2 != 0 ? 'md:order-1' : 'md:order-2'">
+              {{ step.name }}
+            </a>
             <span class=" text-white text-nowrap font-semibold" 
-            :class="index % 2 != 0 ? 'md:order-2' : 'md:order-1'">{{ step.start + ' - ' + step.end }}</span>
+            :class="index % 2 != 0 ? 'md:order-2' : 'md:order-1'">
+              {{ step.start + ' - ' + step.end }}
+            </span>
           </div>
 
           <p class="text-white-grey text-lg mt-4" :class="index % 2 != 0 ? 'md:text-start' : 'md:text-end'">
