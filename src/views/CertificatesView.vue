427482115c5f5cde5b0b8db9344fd3ec17a3fc70
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-05-08 15:50:18 +0200

    Filter for years

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -6,11 +6,23 @@
   <h2 class="text-white text-center font-della text-lg mb-10"> 
     He dedicado gran parte de mi trayectoria a formarme de manera constante y exhaustiva. En esta sección podrás encontrar los certificados que respaldan mi experiencia profesional y mi compromiso con el aprendizaje continuo.
   </h2>
-  <div class="p-8 rounded-3xl bg-cyan-700 w-1/2 justify-self-center mb-12">
 
+  <div class="flex p-4 rounded-full bg-cyan-700 w-1/2 justify-self-center mb-12 justify-around">
+    <span 
+      v-for="year, index in years" 
+      :key="index" 
+      class="py-4 px-8 font-semibold rounded-full cursor-pointer" 
+      :class="selectedYear === year ? 'font-bold shadow-2xl bg-white' : ''"
+      @click="selectedYear != 'all' ? selectedYear = 'all'  : orderCertificates(year)">
+      {{ year }}
+    </span>
+    <span v-if="selectedYear != 'all'">
+      🗑
+    </span>
   </div>
+
   <div class="grid grid-cols-4 justify-between justify-items-center">
-    <div v-for="title in certificates" :key="title.year" class="rounded-lg mb-8 pa-4 block">
+    <div v-for="title in filteredCertificates" :key="title.year" class="rounded-lg mb-8 pa-4 block">
         <h1>
           {{ title.year }}
         </h1>
@@ -32,16 +44,13 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { years } from '@/constants/data';
+
 
 // Data
 const certificates = ref([
-  {
-    2023: [
-      {title: 'Javascript', year: '2018', src: "src/assets/titles/JavaScript.png"}
-    ]
-  },
-  
+  {title: 'Javascript', year: '2018', src: "src/assets/titles/JavaScript.png"},
   {title: 'Desarrollo Web I', year: '2019', src: "src/assets/titles/WebDev1.png"},
   {title: 'Desarrollo de Apps Moviles', year: '2019', src: "src/assets/titles/AppsMoviles.png"},
   {title: 'Básicos de Android', year: '2020', src: "src/assets/titles/BasicsAndroid.png"},
@@ -54,5 +63,18 @@ const certificates = ref([
   {title: 'Desarrollo con Vue.js y Laravel', year: '2023', src: "src/assets/titles/Vue&Laravel MasterMind.png"},
   {title: 'Desarrollo con Vue.js', year: '2023', src: "src/assets/titles/Vue.jsMasterMind.png"},
 ])
+
+let selectedYear = ref('all')
+
+// Methods
+function orderCertificates(year) {
+  selectedYear.value = year
+}
+
+const filteredCertificates = computed(() => {
+    return selectedYear.value === 'all'
+    ? certificates.value
+    : certificates.value.filter(certificate => certificate.year === selectedYear.value)
+})
 
 </script>
