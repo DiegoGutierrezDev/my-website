18955e31447b69bfd7846ba717557c22453fc14b
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-05-21 13:10:32 +0200

    Button functionality for removing filters

--- src/views/CertificatesView.vue
+++ src/views/CertificatesView.vue
@@ -24,12 +24,15 @@
       leave-from-class="opacity-100 scale-y-100 origin-top"
       leave-to-class="opacity-0 scale-y-0 origin-top"
      >
-    <div v-if="selectedYear != 'all'" class="bg-cyan-700 w-10 h-12 rounded-b-lg absolute -bottom-10 justify-center items-center flex z-10">
+    <button 
+      v-if="selectedYear != 'all'" 
+      class="bg-cyan-700 w-10 h-12 rounded-b-lg absolute -bottom-10 justify-center items-center flex z-10"
+      @click="selectedYear = 'all'">
       <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-trash" viewBox="0 0 16 16">
         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
       </svg>
-    </div>
+    </button>
     </transition>
   </div>
 
