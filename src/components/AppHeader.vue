3f98f07049482c0a7bac7d9d3fa7afbb93f12e6e
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-06-25 17:57:25 +0200

    Adding links to main page

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,9 +14,16 @@
       <div
         class="w-96 flex max-sm:p-4 justify-around items-center font-hind text-white"
       >
-        <router-link :to="{ hash: '#carreer' }">Mi Carrera</router-link>
-        <router-link :to="{ hash: '#stack' }">Mis Skills</router-link>
-        <router-link :to="{ hash: '#contact' }">Contacto</router-link>
+        <router-link :to="{ path: '/', hash: '#carreer' }"
+          >Mi Carrera</router-link
+        >
+        <router-link :to="{ path: '/', hash: '#stack' }"
+          >Mis Skills</router-link
+        >
+        <router-link to="/certificates">Mis Certificados</router-link>
+        <router-link :to="{ path: '/', hash: '#contact' }"
+          >Contacto</router-link
+        >
       </div>
     </div>
   </header>
